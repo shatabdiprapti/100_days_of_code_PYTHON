56ff984080bf075291b0e1c2c6a441be26231594
Shatabdi Sen Prapti <2008038@ipe.buet.ac.bd>
2024-08-30 23:06:01 +0600

    Add files via upload

---
+++ day_01/Band Generator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator")
+
+# Get user input
+city_name = input("What's the name of the city you grew up in?: ")
+pet_name = input("What's your pet's name?: ")
+
+# Print the result with proper formatting
+print(f"Your band name could be: {city_name} {pet_name}")
